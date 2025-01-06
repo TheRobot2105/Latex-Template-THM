817df6731db5c3a609fc6b5990233af6b558c7e5
Felix Kimmel <felix.kimmel@web.de>
2025-01-06 01:22:21 +0100

    grobe Struktur erstellt

--- buildPDFLinux.sh
+++ buildPDFLinux.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/env bash
+
+INPUT=./report.tex
+TEMP=./tmp
+echo "Hello World"
+if [[-d "$TEMP"]];then
+    rm -rf "$TEMP"
+fi
+
+mkdir "$TEMP"
+
+xelatex -output-directory=$TEMP -shell-escape "$INPUT"
+biber --input-directory=$TEMP "$INPUT"
+makeglossaries -d $TEMP $INPUT
+xelatex -output-directory=$TEMP -shell-escape "$INPUT"
+xelatex -output-directory=$TEMP -shell-escape "$INPUT"
